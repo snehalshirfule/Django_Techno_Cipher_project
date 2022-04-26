a63571127e275aef8184771c8ea0b28ad2d8f553
abhijadhav128 <92686851+abhijadhav128@users.noreply.github.com>
2022-04-26 14:51:56 +0530

    changes in view added two more new views addpost and allpost

---
+++ urls.py
@@ -0,0 +1,16 @@
+
+
+from django.urls import path
+from vendor import views
+
+urlpatterns = [
+
+    path('signin',views.signin),
+    path('signup',views.signup),
+    path('vdashboard',views.vdashboard),
+    path('profile',views.saveprofile),
+    path('signout',views.signout),
+    path('mypackage',views.mypackage),
+    path('post',views.addpost),
+    path('album',views.allpost),
+]
